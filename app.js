4bae0c13590bac69402499c45ece9cbedd08af9f
Rafael A Palomares Linares <rpalomareslinares@gmail.com>
2024-11-08 09:17:25 +0100

    Index y motor de vistas

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const db = require('./db');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
 
 const app = express();
 const port = process.env.SERVICE_PORT || 8000;
+
+
+
+// Motor de vistas Pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`)
@@ -11,6 +23,6 @@ app.listen(port, () => {
 
 
 app.get('/', (req, res) => {
-    res.send("Bienvenido al Concesionario los Pallos")
+    res.render('index')
 })
 
