d83ece5c2ca53a492c481027e3bd254cef87b407
Rafael A Palomares Linares <rpalomareslinares@gmail.com>
2024-11-11 10:02:02 +0100

    refactor controller

--- controllers/vehiculoController.js
+++ controllers/vehiculoController.js
@@ -10,7 +10,6 @@ exports.listarVehiculos = (req, res) => {
   db.query("SELECT * FROM `Vehiculo`", (err, response) => {
     if (err) res.send("ERROR al hacer la consulta");
     else res.render("vehiculos/list", { vehiculos: response });
-    console.log(response)
   });
 }
 
