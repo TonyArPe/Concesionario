bcd0bb989acc5793c508404ddb6c4080b257d781
Tony Aragon <aaraper747.g.educaand.es>
2024-11-11 12:24:31 +0100

    Arreglo Routers

--- routes/ventaRouter.js
+++ routes/ventaRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ventasController = require('../controllers/compraController');
+const ventasController = require('../controllers/ventaController');
 
 
 router.get('/', ventasController.listarVentas);
